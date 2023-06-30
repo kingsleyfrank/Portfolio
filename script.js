10e2eaf6810735b89fe8c4e28d4c273da456876c
Kingsleyfrank <kfakrogha@gmail.com>
2023-06-30 11:11:05 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Web Developer", "Blogger", "Social Media Manager"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
